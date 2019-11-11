b665f2a16ed4bfd908a2154c35e934856e227639
gbs4ever <gbs4ever2@gmail.com>
2019-11-11 18:19:34 -0500

    removed blank href

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom';
-import { logout } from '../actions/currentUser.js'
-const Logout = ({history, logout }) => {
-  const handleSubmit = (event) => {
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { logout } from "../actions/currentUser.js";
+const Logout = ({ history, logout }) => {
+  const handleSubmit = event => {
     event.preventDefault();
-    logout()
-      .then(history.push('/login'));
+    logout().then(history.push("/login"));
   };
-   
-    return (
-     <div className="LogoutButton">
-     
-        
-      <a href="#" rel="noopener noreferrer"  onClick={handleSubmit}>Logout</a> 
-    
-     </div>
-    )
-  }
 
+  return (
+    <div className="LogoutButton">
+      <a href="#top" rel="noopener noreferrer" onClick={handleSubmit}>
+        Logout
+      </a>
+    </div>
+  );
+};
 
-
-export default withRouter(connect(null, {logout})(Logout))
+export default withRouter(connect(null, { logout })(Logout));
