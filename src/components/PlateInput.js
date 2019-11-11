60ee0aa6220ada39ad7438db8061a9a4738f6617
gbs4ever <gbs4ever2@gmail.com>
2019-11-11 16:25:11 -0500

    changed plate input bootstap

--- src/components/PlateInput.js
+++ src/components/PlateInput.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { search} from '../actions/plate.js'
-import { withRouter} from "react-router-dom";
-import PlatesContainer from '../container/PlatesContainer.js'
-import { MDBContainer,MDBBtn } from 'mdbreact';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { search } from "../actions/plate.js";
+import { withRouter } from "react-router-dom";
+import PlatesContainer from "../container/PlatesContainer.js";
+import { MDBContainer, MDBBtn } from "mdbreact";
 function validate(number, state) {
   // true means invalid, so our conditions got reversed
   return {
@@ -15,75 +15,89 @@ class PlateInput extends Component {
   state = {
     number: "",
     state: ""
-  }
-    
+  };
 
-  onSubmit = (event) => {
-    event.preventDefault()
-  
-    this.props.search(this.state)
-      .then(data => {
-        if(data.error){
-          alert(data.error)
-        } else if(data.notice) {
-          alert(data.notice)
-        } else {
-         this.props.history.push('/violations')
-        }
-      })
-      
+  onSubmit = event => {
+    console.log(this.state, "test");
+    event.preventDefault();
+
+    this.props.search(this.state).then(data => {
+      if (data.error) {
+        alert(data.error);
+      } else if (data.notice) {
+        alert(data.notice);
+      } else {
+        this.props.history.push("/violations");
+      }
+    });
 
     this.setState({
       number: "",
-      state: "" })
+      state: ""
+    });
   };
 
-
-
-  onChange = (event) => {
+  onChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-   
-    })
-   
-
-  }
-
+    });
+  };
 
   render() {
-   
-  const errors = validate(this.state.number, this.state.state);
-  const isDisabled = Object.keys(errors).some(x => errors[x]);
+    const errors = validate(this.state.number, this.state.state);
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
     //const isEnabled = this.state.number.length > 0 && this.state.state.length > 1;
     return (
-   
-      <div className ="col-centered">
-     
+      <div className="col-centered">
         <fieldset className="boxcenter">
           <strong>Please enter your information</strong>
-        <form className="boxed" onSubmit={this.onSubmit}>
-            <p className="h4 text-center mb-4"> </p>
-            <label icon="envelope" htmlFor="defaultFormLoginEmailEx" className="blue-text">
-                      Your email:
-          </label>
-            <input className="form-control" id={errors.number ? "error" : ""}
-           onChange={this.onChange} type="text" name="number" value={this.state.number} /> <br/>
-            <label icon="envelope" htmlFor="defaultFormLoginEmailEx" className="blue-text">State</label>
-            <input className="form-control" id={errors.state ? "error" : ""}
-          onChange={this.onChange} type="text" name="state" value={this.state.state} /> <br />
-            <MDBBtn  disabled={isDisabled}>Search  </MDBBtn>
-        </form>
+          <MDBContainer onSubmit={this.onSubmit}>
+            <form className="boxed">
+              <p className="h4 text-center mb-4"> </p>
+              <label
+                icon="envelope"
+                htmlFor="defaultFormLoginEmailEx"
+                className="blue-text"
+              >
+                Plate:
+              </label>
+              <input
+                className="form-control"
+                id={errors.number ? "error" : ""}
+                onChange={this.onChange}
+                type="text"
+                name="number"
+                value={this.state.number}
+              />{" "}
+              <br />
+              <label
+                icon="envelope"
+                htmlFor="defaultFormLoginEmailEx"
+                className="blue-text"
+              >
+                State
+              </label>
+              <input
+                className="form-control"
+                id={errors.state ? "error" : ""}
+                onChange={this.onChange}
+                type="text"
+                name="state"
+                value={this.state.state}
+              />
+              <br />
+              <button id="button center" disabled={isDisabled}>
+                Search{" "}
+              </button>
+            </form>
+          </MDBContainer>
         </fieldset>
-        <div className = "plates">
+        <div className="plates">
           <PlatesContainer />
-
         </div>
       </div>
-   
-        
-      
-    )
+    );
   }
 }
 
-export default withRouter(connect(null, { search})(PlateInput)) 
+export default withRouter(connect(null, { search })(PlateInput));
