f4abe18d15934a1023948416ff992fde5890d5d2
gbs4ever <gbs4ever2@gmail.com>
2019-11-12 12:07:08 -0500

    add  tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, unmountComponentAtNode } from "react-dom";
 import App from "./App";
+import LoginForm from "./components/LoginForm";
 import { Provider } from "react-redux";
+import { act as domAct } from "react-dom/test-utils";
+import { act as testAct, create } from "react-test-renderer";
+let container = null;
+//run before each test
+beforeEach(() => {
+  container = document.createElement("root");
+  document.body.appendChild(container);
+});
 
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+//tests
 it("renders without crashing", () => {
-  const div = document.createElement("root");
   <Provider>
     <App />
   </Provider>;
-
-  ReactDOM.unmountComponentAtNode(div);
+});
+//tests
+it("check that login form loads", () => {
+  <LoginForm></LoginForm>;
 });
